a07101523e60f8b5385a482b30d5d2c1f1310719
GibranEtiel <gib.beiza@duocuc.cl>
2024-07-30 08:16:52 +0200

    ddd

--- assent/js/panda.js
+++ assent/js/panda.js
@@ -57,7 +57,7 @@ document.addEventListener("click", (e) => {
 });
 function validatePassword() {
     var passwordInput = document.getElementById("password").value;
-    if (passwordInput !== "dantiar") {
+    if (passwordInput !== "DANTIAR") {
       var errorMessage = document.getElementById("errorMessage");
       errorMessage.textContent = "La contraseña es incorrecta.";
       return false; // Evita el envío del formulario si la contraseña es incorrecta
